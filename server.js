d7edfd7eeca20ff3f822c41d0d331d42d6d73435
naeemcodemaster <onlinecodemaster@gmail.com>
2025-11-28 00:19:15 +0500

    package json verions updated

--- server.js
+++ server.js
@@ -1,2 +1,4 @@
-const { pluck } = require('naeem-testing-lib');
-console.log(pluck([{ name: 'naeem', age: 24 }, { name: 'ali', age: 30 }], 'name'));
+const {pluck} = require('naeem-testing-lib');
+
+console.log(pluck([{ name: 'naeem', age: 24 }, { name: 'ali', age: 30 }], 'age'));
+
